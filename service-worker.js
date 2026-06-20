37d2978b32671c1804eb2a0e9084b7798e3ba2b2
João Caldas <joaoccaldas@gmail.com>
2026-06-20 02:14:36 +0200

    chore: cache final Wordbound application shell

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE='wordbound-whispering-woods-v1';
-const SHELL=['./','./index.html','./manifest.webmanifest','./css/wordbound.css','./js/wordbound/data.js','./js/wordbound/engine.js','./js/wordbound/app.js','./assets/wordbound-mark.svg','./assets/whispering-woods.svg'];
+const SHELL=['./','./index.html','./manifest.webmanifest','./css/wordbound.css','./js/wordbound/data.js','./js/wordbound/engine.js','./js/wordbound/app.js','./js/wordbound/safety.js','./assets/wordbound-mark.svg','./assets/whispering-woods.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));});
